a87c3d8b3a88ec478a80856460c92fc3b31e6b16
jonsen <im16hot@gmail.com>
2013-02-24 07:15:30 +0800

    add file

---
+++ mail.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"net/mail"
+	"net/smtp"
+	"time"
+    "strings"
+    "log"
+)
+
+type Mailer struct {
+    Subject string
+    Content string
+    To string
+}
+
+var mailChan = make(chan *Mailer)
+
+// 运行一个goroutin 监听发生邮件任务
+func MailSendServer() {
+//    mailChan = make(chan *Mailer)
+    log.Println( "Running Mail Send Server..." )
+
+	for {
+
+		mail := <-mailChan
+
+		if mail == nil {
+			continue
+		}
+
+        m := NewMailMessage( mail.Subject, mail.Content, mail.To )
+        if err := m.Send(); err != nil {
+            log.Println( "send mail to " + mail.To + " error %s", err )
+        }
+	}
+
+}
+
+func MailSender( subject, content, to string ) ( err error ) {
+
+    if subject != "" && content != "" && to != "" {
+        if AppConfig.smtpDaemon {
+            mailChan <- &Mailer{ Subject: subject, Content: content, To: to }
+        } else {
+            m := NewMailMessage( subject, content, to )
+            if err = m.Send(); err != nil {
+                log.Println( "send mail to " + to + " error %s", err )
+                return err
+            }
+        }
+
+        return nil
+    }
+
+    return errors.New("input is null")
+}
+
+
+func NewMailMessage(subject, content, to string) *MailMessage {
+    tos := strings.Split( to, "," )
+	message := &MailMessage{Subject: subject, Content: content,
+        To: make([]mail.Address, len(tos))}
+
+	for k, v := range tos {
+		message.To[k].Address = v
+	}
+
+    //fmt.Println( message.To )
+	return message
+}
+
+func NewMailMessageFrom(subject, content, from, to string) *MailMessage {
+	message := NewMailMessage(subject, content, to)
+	message.From.Address = from
+	return message
+}
+
+const crlf = "\r\n"
+
+type MailMessage struct {
+	From    mail.Address // if From.Address is empty, Config.DefaultFrom will be used
+	To      []mail.Address
+	Cc      []mail.Address
+	Bcc     []mail.Address
+	Subject string
+	Content string
+}
+
+// http://tools.ietf.org/html/rfc822
+// http://tools.ietf.org/html/rfc2821
+func (self *MailMessage) String() string {
+	var buf bytes.Buffer
+
+	write := func(what string, recipients []mail.Address) {
+		if len(recipients) == 0 {
+			return
+		}
+		for i := range recipients {
+			if i == 0 {
+				buf.WriteString(what)
+			} else {
+				buf.WriteString(", ")
+			}
+			buf.WriteString(recipients[i].String())
+		}
+		buf.WriteString(crlf)
+	}
+
+	from := &self.From
+	if from.Address == "" {
+		from = &mail.Address{ "domainpark", AppConfig.smtpUser }//&Config.From
+	}
+
+    /*if AppConfig.adminMail != "" {
+        self.Bcc = make([]mail.Address, 1)
+        self.Bcc[0] = mail.Address{ "adminer", AppConfig.adminMail }
+    }*/
+
+
+	fmt.Fprintf(&buf, "From: %s%s", from.String(), crlf)
+	write("To: ", self.To)
+	write("Cc: ", self.Cc)
+	write("Bcc: ", self.Bcc)
+	fmt.Fprintf(&buf, "Date: %s%s", time.Now().UTC().Format(time.RFC822), crlf)
+	fmt.Fprintf(&buf, "Subject: %s%s%s", self.Subject, crlf, self.Content)
+	return buf.String()
+}
+
+// Returns the first error
+func (self *MailMessage) Validate() error {
+	if len(self.To) == 0 {
+		return errors.New("Missing email recipient (email.Message.To)")
+	}
+	return nil
+}
+
+type fakeAuth struct {
+    smtp.Auth
+}
+
+func (a fakeAuth) Start(server *smtp.ServerInfo) (string, []byte, error) {
+    server.TLS = true
+    return a.Auth.Start(server)
+}
+
+
+func (self *MailMessage) Send() error {
+    var auth smtp.Auth
+
+	if err := self.Validate(); err != nil {
+		return err
+	}
+
+    to := make([]string, len(self.To))
+	for i := range self.To {
+		to[i] = self.To[i].Address
+	}
+
+    from := self.From.Address
+	if from == "" {
+		from = AppConfig.smtpUser// Config.From.Address
+	}
+
+    addr := fmt.Sprintf("%s:%d", AppConfig.smtpHost, AppConfig.smtpPort)
+
+    if AppConfig.smtpTLS {
+        auth = fakeAuth{smtp.PlainAuth("", AppConfig.smtpUser,
+            AppConfig.smtpPassword, AppConfig.smtpHost)}
+    } else {
+        auth = smtp.PlainAuth("", AppConfig.smtpUser, AppConfig.smtpPassword,
+            AppConfig.smtpHost)
+    }
+
+	return smtp.SendMail(addr, auth, from, to, []byte(self.String()))
+}
---
+++ main.go
@@ -0,0 +1,230 @@
+
+package main
+
+import (
+	"fmt"
+	//"io/ioutil"
+	//"net"
+	"net/http"
+    "html/template"
+    "flag"
+	"strings"
+    "os"
+    "time"
+    //"strings"
+    redis "github.com/alphazero/Go-Redis"
+    config "github.com/jonsen/goconfig/config"
+)
+
+const (
+	cfgFileDefault = "./serverd.conf"
+)
+
+type sysCfg struct {
+	// SiteName string
+    logfile string
+	webPort, debug int
+
+    // redis setup
+	redisHost, redisAuth, redisKeyFix string
+	redisPort, redisDb int
+
+    // SMTP setup
+    smtpHost, smtpUser, smtpPassword string
+    smtpPort int
+    smtpAuth, smtpTLS, smtpDaemon bool
+
+}
+
+var AppConfig sysCfg
+
+
+// Read cfgfile or setup defaults.
+func setupConfig() {
+	// 初始化读取配置文件
+	c, err := config.ReadDefault( *cfgfile )
+	if err != nil {
+		fmt.Println( "read config file error: ", *cfgfile, err )
+		os.Exit(1)
+	}
+
+	// 读取[common]段配置
+
+	AppConfig.webPort, _ = c.Int("common", "webport")
+	AppConfig.debug, _ = c.Int("common", "debug")
+	AppConfig.logfile, _ = c.String("common", "log")
+
+	AppConfig.redisHost, _ = c.String("redis", "host")
+	AppConfig.redisAuth, _ = c.String("redis", "auth")
+	AppConfig.redisKeyFix, _ = c.String("redis", "key_prefix")
+	AppConfig.redisPort, _ = c.Int("redis", "prt")
+	AppConfig.redisDb, _ = c.Int("redis", "db")
+
+    // SMTP
+	AppConfig.smtpHost, _ = c.String("smtp", "host")
+	AppConfig.smtpUser, _ = c.String("smtp", "user")
+	AppConfig.smtpPassword, _ = c.String("smtp", "password")
+	AppConfig.smtpPort, _ = c.Int("smtp", "port")
+	AppConfig.smtpAuth, _ = c.Bool("smtp", "auth")
+	AppConfig.smtpTLS, _ = c.Bool("smtp", "tls")
+    AppConfig.smtpDaemon, _ = c.Bool("smtp", "daemon" )
+
+}
+
+func redisConnect() ( rd redis.Client, err error ) {
+	spec := redis.DefaultSpec().Db(AppConfig.redisDb)
+	if AppConfig.redisHost != "" {
+		spec.Host( AppConfig.redisHost )
+	}
+	if AppConfig.redisAuth != "" {
+		spec.Password( AppConfig.redisAuth )
+	}
+	port := AppConfig.redisPort
+	if port > 0 && port < 65535 {
+		spec.Port( port )
+	}
+
+	rd, err = redis.NewSynchClientWithSpec(spec)
+	if err != nil {
+        fmt.Printf( "Connect Redis: %s", err )
+        return rd, err
+	}
+
+	return rd, nil
+}
+
+
+func rediClose() {
+
+}
+
+
+func report() (rep string, err error) {
+    rd, err := redisConnect()
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    defer rd.Quit()
+
+    date := time.Now().Format("20060102")
+    keys := "*:" + date
+
+    rep = "HI\n"
+    rep += "    This report for " + date + "\n"
+    rep += "    Domain and count:\n\n"
+
+    list, err := rd.Keys( keys )
+	if err != nil {
+		return "", err
+	}
+	// fmt.Println(list)	
+
+	for _, v := range list {
+        count, _ := rd.Get(v)
+        rep += "    " + v + ": " + string(count) + "\n";
+    }
+
+    return rep, nil
+
+
+}
+
+func reportServer() {
+    var day int
+
+    fmt.Println("running report server ...")
+
+    for {
+        currDay := time.Now().Day()
+        //fmt.Println( currDay, day )
+        if day != currDay {
+            // execute send mail ...
+            fmt.Println("send mail now ...")
+            reportBody, err := report()
+            if err == nil {
+                err = MailSender( "Domain Park Report", reportBody, "im16hot@gmail.com" )
+                if err != nil {
+                    fmt.Println("send mail err", err )
+                } else {
+                    //fmt.Println("send mail ok")
+                }
+            }
+
+            day = currDay
+        }
+        time.Sleep( 30 * time.Minute )
+    }
+
+}
+
+/**
+ * 连接入口
+ *
+ * author: jonsen yang
+ * date: 2012-10-17
+ */
+func makeHandler(w http.ResponseWriter, r *http.Request) {
+    if r.RequestURI == "/favicon.ico" || r.RequestURI == "/robots.txt" {
+        //http.NotFound(w,r)
+        return
+    }
+    fmt.Printf( "[%v] %s %s %s %s %v\n",time.Now(), r.RemoteAddr, r.Method, r.Host, r.RequestURI, r.Referer()  )
+
+    t, _ := template.ParseFiles("index.html")
+    t.Execute(w, nil)
+
+
+
+    hosts := strings.Split( r.Host, ":" )
+//    fmt.Println(r.Host, hosts, r.RequestURI )
+//    fmt.Println( getDomainRoot( hosts[0] ) )
+
+
+    domain, rootid := getDomainRoot( strings.ToLower( hosts[0] ))
+    if domain == "" || rootid < 1 {
+        //fmt.Println( domain, rootid )
+        return
+    }
+
+    rd, err := redisConnect()
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    date := time.Now().Format("20060102")
+    fmt.Println( domain + ":" + date)
+
+    key := domain + ":" + date
+    rd.Incr( key )
+
+    rd.Quit()
+
+}
+
+
+
+var cfgfile = flag.String("c", cfgFileDefault, "Config file")
+
+
+func main() {
+    flag.Parse()
+    setupConfig()
+    initDomainExt()
+
+    go reportServer()
+
+    listen := fmt.Sprintf(":%d", AppConfig.webPort )
+
+	http.HandleFunc( "/", makeHandler )
+    err := http.ListenAndServe( listen, nil )
+    if err != nil {
+        fmt.Println( "Run server err", err )
+        os.Exit(1)
+    }
+
+}
+
+
